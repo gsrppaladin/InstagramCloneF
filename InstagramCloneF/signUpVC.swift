d878eceee74e9c76d00e06fd6ed77781819bb003
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-09-17 17:32:43 -0400

    Created the sign in page to the app

---
+++ InstagramCloneF/signUpVC.swift
@@ -0,0 +1,39 @@
+//
+//  signUpVC.swift
+//  InstagramCloneF
+//
+//  Created by Sam Greenhill on 9/17/17.
+//  Copyright © 2017 simplyAmazingMachines. All rights reserved.
+//
+
+import UIKit
+
+class signUpVC: UIViewController {
+
+    
+    @IBOutlet var emailTxtField: UITextField!
+    
+    @IBOutlet var passwordTxt: UITextField!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInPressed(_ sender: Any) {
+    }
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+
+}
